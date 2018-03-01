8212fc24ee85cac72943204f0024cb3e2eebc61c
wesalnama111 <wesal.iintellect@gmail.com>
2018-03-01 15:35:48 +0500

    conditionalredirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 //import axios from '../../axios';
 import Posts from './Posts/Posts';
-import {Route,NavLink,Switch} from 'react-router-dom';
+import {Route,NavLink,Switch,Redirect} from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 
@@ -44,7 +44,9 @@ class Blog extends Component {
             <Switch>
                  <Route path="/new-post"  component={NewPost}/>
            
-                <Route path="/posts/"  component={Posts}/>
+                 <Route path="/posts/"  component={Posts}/>
+                 <Redirect from ="/" to="/posts" />
+                 {/* <Route path="/"  component={Posts}/> */}
             
             </Switch>
             </div>
