a8f46a005c114734d0f31a4356ad793df1b97d5c
wesalnama111 <wesal.iintellect@gmail.com>
2018-03-01 13:05:06 +0500

    routingdynamically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 import {Route,NavLink,Switch} from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 
 class Blog extends Component {
@@ -15,13 +14,14 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                             to="/" 
-                             exact activeClassName="my-active"
+                             to="/posts/" 
+                            exact
+                              activeClassName="my-active"
                              activeStyle={{
                                 color: '#fa923f',
                                 textDecoration: 'underline'
                              }}>
-                            Home</NavLink></li>
+                            Posts</NavLink></li>
                             <li>
                                 <NavLink to={{                            
                                 //pathname: this.props.match.url+ '/new-post',
@@ -40,12 +40,13 @@ class Blog extends Component {
             <Route path="/"  render={()=>
             
             <h1>Home taken2 </h1>}/> */}
-             <Route path="/" exact component={Posts}/>
                
             <Switch>
-                <Route path="/new-post"  component={NewPost}/>
-                <Route path="/:id" exact component={FullPost}/>
-                </Switch>
+                 <Route path="/new-post"  component={NewPost}/>
+           
+                <Route path="/posts/"  component={Posts}/>
+            
+            </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -3,7 +3,8 @@ import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 //import {Link} from 'react-router-dom';
 import './Posts.css';
-
+import {Route} from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component{
     state={
@@ -31,15 +32,15 @@ class Posts extends Component{
      }
    
       postSelectedHandler = (id) => {
-        //this.props.history.push({pathname:'/'  +id});
-        this.props.history.push('/'  + id);
+        //this.props.history.push({pathname:'/posts/'  +id});
+        this.props.history.push('/posts/'  + id);
 }
     render(){
         let posts =<p style={{textAlign: 'center'}}>somthing went wrong</p>;
         if(!this.state.error){
              posts=this.state.posts.map(post=>{
                 return (
-                //<Link to={'/' +post.id} >
+                //<Link to={'/posts/' +post.id} >
                  <Post                    
                     key={post.id}
                     title={post.title}
@@ -54,9 +55,12 @@ class Posts extends Component{
         
         }
         return(
+        <div>
             <section className="Posts">
             {posts}
-         </section>
+            </section>
+            <Route path={this.props.match.url+'/:id'}   exact component={FullPost}/>    
+        </div>
         );
 
     }
